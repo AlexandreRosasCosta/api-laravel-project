c09c52281da03589c4c96d3604c6c401c11fb469
Alexandre <alexandrerosascosta@gmail.com>
2023-06-22 22:27:06 -0300

    Feat: Adição UsersController e atualização do modelo de senha

--- database/factories/UsersFactory.php
+++ database/factories/UsersFactory.php
@@ -22,7 +22,7 @@ class UsersFactory extends Factory
             'name' => $this->faker->unique()->word,
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi',
             'birth_date' => fake()->date()
         ];
     }
